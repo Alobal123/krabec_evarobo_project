f91f8ec16e50dca32b5395443506ca7b5c18e9e3
Alobal123 <miroslavkrabec@seznam.cz>
2018-05-18 13:02:45 +0200

    Fixed collisions

--- Simulate.py
+++ Simulate.py
@@ -4,19 +4,21 @@ import Stairs
 import Robot
 
 def simulate():
-    sim = pyrosim.Simulator(eval_time=1000, debug=True, xyz = [-2,0,1], hpr=[0,-27.5,0.0])
+    sim = pyrosim.Simulator(eval_time=1000, debug=False, xyz = [-2,0,1], hpr=[0,-27.5,0.0])
     builder = Stairs.StairBuilder(sim,[1,0,0],0.2)
     builder.build(20)
     
-    
-    num_sensors = 5
+    num_sensors = 4
     num_motors = 8
     weight_matrix = np.random.rand(num_sensors+num_motors,
                                    num_sensors+num_motors, 4)
     weight_matrix[:, :, 0:1] = weight_matrix[:, :, 0:1]*2.-1.
+    print(weight_matrix)
     
     robot = Robot.Robot(sim,weight_matrix)
     robot.build()
+    
+    sim.create_collision_matrix('intra')
     sim.start()
     
     
