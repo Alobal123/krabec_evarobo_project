833ffe3c879401a85fb3ba5a0799669d636c9310
Alobal123 <miroslavkrabec@seznam.cz>
2018-05-19 14:59:58 +0200

    Added sensors to joints

--- Robot.py
+++ Robot.py
@@ -16,7 +16,7 @@ class Robot:
                              length=self.Height, width=self.Height,
                              height=self.EPS*2.0, mass=1, collision_group='robot')
         
-        positions_sensor = self.simulator.send_position_sensor(main_body)
+
         
         # id arrays
         thighs = [0]*4
@@ -24,7 +24,9 @@ class Robot:
         hips = [0]*4
         knees = [0]*4
         foot_sensors = [0]*4
-        sensor_neurons = [0]*5
+        hip_sensors = [0]*4
+        knee_sensors = [0]*4
+        sensor_neurons = [0]*12
         motor_neurons = [0]*8
     
         delta = float(math.pi)/2.0
@@ -47,7 +49,8 @@ class Robot:
                                            n1=-y_pos, n2=x_pos, n3=0,
                                            lo=-math.pi/4.0, hi=math.pi/4.0,
                                            speed=1.0)
-    
+            hip_sensors[i] = self.simulator.send_proprioceptive_sensor(hips[i])
+            sensor_neurons[3*i] = self.simulator.send_sensor_neuron(hip_sensors[i])
             motor_neurons[i] = self.simulator.send_motor_neuron(joint_id=hips[i])
     
             x_pos2 = math.cos(theta)*1.5*self.Height
@@ -63,26 +66,30 @@ class Robot:
                                             n1=-y_pos, n2=x_pos, n3=0,
                                             lo=-math.pi/4.0, hi=math.pi/4.0)
     
+            knee_sensors[i] = self.simulator.send_proprioceptive_sensor(knees[i])
+            sensor_neurons[3*i+1] = self.simulator.send_sensor_neuron(knee_sensors[i])
+            
             motor_neurons[i+4] = self.simulator.send_motor_neuron(knees[i])
             foot_sensors[i] = self.simulator.send_touch_sensor(shins[i])
-            sensor_neurons[i] = self.simulator.send_sensor_neuron(foot_sensors[i])
+            sensor_neurons[3*i+2] = self.simulator.send_sensor_neuron(foot_sensors[i])
     
         # developing synapses linearly change from the start value to the
         # end value over the course of start time to end time
         # Here we connect each sensor to each motor, pulling weights from
         # the weight matrix
-        for source_id in sensor_neurons:
-            for target_id in motor_neurons:
+        for source_id in range(len(sensor_neurons)):
+            for target_id in range(len(motor_neurons)):
 
                 start_weight = self.weight_matrix[source_id, target_id, 0]
                 end_weight = self.weight_matrix[source_id, target_id, 1]
                 start_time = self.weight_matrix[source_id, target_id, 2]
                 end_time = self.weight_matrix[source_id, target_id, 3]
-                self.simulator.send_developing_synapse(source_id, target_id,
+                self.simulator.send_developing_synapse(sensor_neurons[source_id], motor_neurons[target_id],
                                             start_weight=start_weight,
                                             end_weight=end_weight,
                                             start_time=start_time, 
                                             end_time=end_time)
+        positions_sensor = self.simulator.send_position_sensor(main_body)
         return positions_sensor
    
 
